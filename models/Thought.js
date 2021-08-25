dc4cde95db9a2519895d04702171b856c8ad42b7
Biznaz <aldhelm7@gmail.com>
2021-08-25 16:01:00 -0700

    updated mongoose to earlier version to work with node modules

--- models/Thought.js
+++ models/Thought.js
@@ -1,35 +1,6 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-const ThoughtSchema = new Schema(
-  {
-  thoughtText: {
-    type: String,
-    required: true,
-    minLength: 1,
-    maxLength: 280
-
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: createdAtVal => dateFormat(createdAtVal)
-  },
-  username: {
-    type: String,
-    required: true
-  },
-  reactions: [ReactionSchema]
-},
-  {
-    toJSON: {
-      virtuals: true,
-      getters: true
-    },
-    id: false
-  }
-);
-
 const ReactionSchema = new Schema(
   {
     // set custom id to avoid confusion with parent comment _id
@@ -59,6 +30,36 @@ const ReactionSchema = new Schema(
     }
   }
 );
+
+const ThoughtSchema = new Schema(
+  {
+  thoughtText: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 280
+
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: createdAtVal => dateFormat(createdAtVal)
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  reactions: [ReactionSchema]
+},
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
 
 ThoughtSchema.virtual('reactionCount').get(function() {
   return this.reactions.length;
